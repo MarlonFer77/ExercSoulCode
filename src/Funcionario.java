f308ba31529dc4f341f055ddb9bc96e688cc2843
Marlon <105235135+MarlonFer77@users.noreply.github.com>
2022-06-07 09:38:14 -0300

    JavaFunc

---
+++ src/Funcionario.java
@@ -0,0 +1,91 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private String cargo;
+    private String situacao;
+
+    public Funcionario(String nome, int idade, String cargo, String situacao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cargo = cargo;
+        this.situacao = situacao;
+    }
+
+    public Funcionario(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public Funcionario() {
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        Scanner sc = new Scanner(System.in);
+        while (idade <= 0 || idade > 100) {
+            System.out.println("Idade inválida. Digite novamente:");
+            idade = sc.nextInt();
+        }
+        this.idade = idade;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        Scanner sc = new Scanner(System.in);
+        while (!(Objects.equals(situacao, "ativo")) && !(Objects.equals(situacao, "inativo"))) {
+            System.out.println("Opção inválida. Digite (ativo) ou (inativo)");
+            situacao = sc.next();
+        }
+        this.situacao = getSituacao();
+    }
+
+    public String demitirFuncionario() {
+        if (Objects.equals(situacao, "ativo")) {
+            return "foi promovido(a) a cliente";
+        } else if (Objects.equals(situacao, "inativo")) {
+            return "é invativo(a), logo não é mais um(a) funcionário(a)";
+        }
+        return null;
+    }
+
+    public String trocarCargoFuncionario(String novoCargo) {
+        return cargo = novoCargo;
+    }
+
+    public double pagarSalario(double valorHora, double horasTrabalhadas) {
+        if (Objects.equals(getSituacao(), "inativo")) {
+            return 0;
+        } else {
+            return valorHora * horasTrabalhadas;
+        }
+    }
+
+    public int fazerAniversario() {
+        return ++this.idade;
+    }
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira os dados para quatro funcionários:");
+
+        List<Funcionario> func = new ArrayList<Funcionario>();
+        Funcionario padrao = new Funcionario();
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println();
+            System.out.println("Funcionário #" + (i + 1));
+            System.out.println();
+
+            System.out.print("Nome: ");
+            String nome = sc.next();
+
+            System.out.print("Idade: ");
+            int idade = sc.nextInt();
+            padrao.setIdade(idade);
+
+            System.out.print("Cargo: ");
+            sc.next();
+            String cargo = sc.nextLine();
+
+            System.out.print("Situação ");
+            String situacao = sc.next().toLowerCase();
+            padrao.setSituacao(situacao);
+
+            func.add(new Funcionario(nome, idade, cargo, situacao));
+        }
+
+        System.out.println();
+
+        Funcionario demissao = func.get(0);
+        System.out.println("O(A) funcionário(a) " + demissao.getNome() + " " + demissao.demitirFuncionario());
+
+        System.out.println();
+
+        Funcionario promocao = func.get(1);
+        System.out.println("O(A) funcionário(a) " + promocao.getNome() + " foi promovido(a) a " + promocao.trocarCargoFuncionario("Desenvolvedor Pleno"));
+
+        System.out.println();
+
+        Funcionario aniversario = func.get(2);
+        System.out.println("O(A) funcionário(a) " + aniversario.getNome() + " está fazendo " + aniversario.fazerAniversario() + " anos hoje!");
+
+        System.out.println();
+
+        Funcionario salario = func.get(3);
+        System.out.println("Salário de R$" + String.format("%.2f", salario.pagarSalario(20, 100)) + " pago ao(à) funcionário(a) " + salario.getNome());
+
+        sc.close();
+    }
+
+}
